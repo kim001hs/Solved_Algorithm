d0856346b1eb6828d4318a05eaf26fc4c955d69a
KIMHYUNSEO <80082902+kim001hs@users.noreply.github.com>
2024-09-08 00:12:47 +0900

    [Silver IV] Title: 수 찾기, Time: 1636 ms, Memory: 188140 KB -BaekjoonHub

---
+++ Java/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int m=sc.nextInt();
+
+        for(int i = 0; i < m; i++){
+            int temp=sc.nextInt();  
+            int index = Arrays.binarySearch(arr, temp);
+            if (index >= 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+}
