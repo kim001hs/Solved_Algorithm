8e082be49a4d6a78dde15078f0f8c78e434bd242
KIMHYUNSEO <80082902+kim001hs@users.noreply.github.com>
2024-09-08 00:29:19 +0900

    [Silver IV] Title: 카드2, Time: 220 ms, Memory: 49012 KB -BaekjoonHub

---
+++ Java/백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1;i<n+1;i++){
+            q.offer(i);
+        }
+        while(q.size()!=1){
+            q.poll();
+            q.offer(q.poll());
+        }
+        System.out.println(q.poll());
+    }
+}
