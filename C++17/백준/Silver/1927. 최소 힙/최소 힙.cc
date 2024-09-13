fb7048a6add727490e0992f36582044159971e5a
KIMHYUNSEO <80082902+kim001hs@users.noreply.github.com>
2024-09-13 21:18:11 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int x;
+        cin >> x;
+        if(x==0){
+            if(pq.size()){
+                cout << pq.top() << '\n';
+                pq.pop();
+            }else{
+                cout << 0 << '\n';
+            }
+        }else{
+            pq.push(x);
+        }
+    }
+}
