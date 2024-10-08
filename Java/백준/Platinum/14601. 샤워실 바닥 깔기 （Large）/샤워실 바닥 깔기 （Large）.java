d1e23739bf835692bcc372fbeed84c1bb0829cfa
KIMHYUNSEO <80082902+kim001hs@users.noreply.github.com>
2024-10-08 15:06:17 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 492 ms, Memory: 39272 KB -BaekjoonHub

--- Java/백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.java
+++ Java/백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.java
@@ -6,17 +6,22 @@ public class Main{
     static int num=0;
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        //k를 받아 n을 2의 k제곰으로 둔다
         int k=sc.nextInt();
         int n=1;
         for(int i=0;i<k;i++){
             n*=2;
         }
+
         arr = new int[n+1][n+1];
+        //y와x를 받는다
         int x=sc.nextInt();
         int y=n-sc.nextInt()+1;
+        //빈칸에 -1을 넣는다
         arr[y][x]=-1;
+        //재귀함수를 돌린다
         solve(1,1,n);
-
+        //출력
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 System.out.print(arr[i][j]+" ");
@@ -26,14 +31,16 @@ public class Main{
     }
     public static void solve(int y, int x, int size){
         num++;
+        //half_size를 만들고 4등분을 한다
         int half_size=size/2;
+        //4등분중 빈칸이 있는칸을 제외한 세칸이 인접하는 칸에 num을 넣는다
         if(isInside(y, x, half_size))arr[y+half_size-1][x+half_size-1]=num;
         if(isInside(y+half_size, x, half_size))arr[y+half_size][x+half_size-1]=num;
         if(isInside(y, x+half_size, half_size))arr[y+half_size-1][x+half_size]=num;
         if(isInside(y+half_size, x+half_size, half_size))arr[y+half_size][x+half_size]=num;
-
+        //size가 2이면 더이상 나눌수 없으므로 return
         if(size==2)return;
-
+        //다시 4등분을 한다
         solve(y, x, half_size);
         solve(y, x+half_size, half_size);
         solve(y+half_size, x, half_size);
@@ -41,6 +48,7 @@ public class Main{
 
     }
     public static boolean isInside(int y, int x, int size){
+        //받은 정사각형에 빈칸이 있다면 false를 반환
         for(int i=y;i<y+size;i++){
             for(int j=x;j<x+size;j++){
                     if(arr[i][j]!=0){
