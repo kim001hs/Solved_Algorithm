9d157f0fb668d2c51ba99b55fa767e01ca81ddb9
KIMHYUNSEO <80082902+kim001hs@users.noreply.github.com>
2024-10-08 19:35:21 +0900

    [Platinum II] Title: 가장 가까운 두 점, Time: 56 ms, Memory: 3688 KB -BaekjoonHub

--- C++17/백준/Platinum/2261. 가장 가까운 두 점/가장 가까운 두 점.cc
+++ C++17/백준/Platinum/2261. 가장 가까운 두 점/가장 가까운 두 점.cc
@@ -7,46 +7,46 @@ using namespace std;
 
 static vector<pair<int, int>> spots;
 
-int dis(pair<int, int> a, pair<int, int> b) {
+int dis(pair<int, int> a, pair<int, int> b) {//거리의 제곱을 반환
     int x = a.first - b.first;
     int y = a.second - b.second;
-    return x * x + y * y;  // 제곱 거리 계산
+    return x * x + y * y; 
 }
 
-int min(int a, int b, int c) {
+int min(int a, int b, int c) {//세 수 중 최솟값 반환
     return std::min(a, std::min(b, c));
 }
 
 int closest(int start, int end) {
     int size = end - start + 1;
-    if (size == 2) return dis(spots[start], spots[start + 1]);
+    if (size == 2) return dis(spots[start], spots[start + 1]);//사이즈가 2나 3일 때는 바로 거리 최소값 반환
     else if (size == 3) {
         return min(dis(spots[start], spots[start + 1]), dis(spots[start], spots[start + 2]), dis(spots[start + 1], spots[start + 2]));
     }
 
-    int mid = (start + end) / 2;
-    int MIN = min(closest(start, mid), closest(mid + 1, end));
+    int mid = (start + end) / 2;//중간 인덱스
+    int MIN = min(closest(start, mid), closest(mid + 1, end));//좌우 영역에서의 최소값
 
     vector<pair<int, int>> vec;
-    int line = (spots[mid].first + spots[mid + 1].first) / 2;
+    int line = (spots[mid].first + spots[mid + 1].first) / 2;//중간선을 만들어주고 중간선과 x값 차이 제곱이 MIN보다 작은 점들만 벡터에 넣어줌
 
     for (int i = start; i <= end; i++) {  // 범위 수정
-        if (abs(line - spots[i].first) < MIN) {
+        int temp = line - spots[i].first;
+        if (temp*temp < MIN) {
             vec.push_back(spots[i]);
         }
     }
 
     sort(vec.begin(), vec.end(), [](pair<int, int> a, pair<int, int> b) {
-        return a.second < b.second;
+        return a.second < b.second;//y값 기준으로 정렬
     });
 
     for (int i = 0; i < vec.size(); i++) {
         for (int j = i + 1; j < vec.size(); j++) {
             int temp = vec[j].second - vec[i].second;
             int temp2=vec[j].first-vec[i].first;
-            if (temp*temp >= MIN) break;
-            else if(temp2*temp2>=MIN) continue;
-            else MIN = min(MIN, dis(vec[i], vec[j]));
+            if (temp*temp >= MIN) break;//y값 차이 제곱이 MIN보다 크면 break
+            else MIN = min(MIN, dis(vec[i], vec[j]));//아니면 최소값 갱신
         }
     }
     return MIN;
@@ -64,7 +64,7 @@ int main() {
     }
 
     sort(spots.begin(), spots.end(), [](pair<int, int> a, pair<int, int> b) {
-        return a.first < b.first;
+        return a.first < b.first;//x값 기준으로 정렬
     });
 
     cout <<closest(0, n - 1) << endl;
