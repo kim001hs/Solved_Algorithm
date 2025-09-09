7f99ef8e8e2ab0adfe1cf6908828b0b9caaf2abe
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-09 22:06:37 +0900

    [Gold V] Title: A와 B 2, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- PyPy3/백준/Gold/12919. A와 B 2/A와 B 2.py
+++ PyPy3/백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -2,17 +2,17 @@ import sys
 input = sys.stdin.readline
 S=str(input().rstrip())
 T=str(input().rstrip())
-
+#T를 하나씩 줄이고 비교하는 식으로 구현함(왜그랬지)
 def solve(str_):
-    if str_== S:
+    if str_== S:#S와 같아지면 True
         return True
-    if len(str_)<=len(S):
+    if len(str_)<=len(S):#S와 다른데 길이가 같으면 False
         return False
     
-    if str_[-1]=='A':
+    if str_[-1]=='A':#제일 뒤가 A면 없앤버전으로 재귀
         if solve(str_[:-1]):
             return True
-    if str_[0]=='B':
+    if str_[0]=='B':#처음이 B면 뒤집고 B없앤 버전으로 재귀
         if solve(str_[::-1][:-1]):
             return True
     return False
