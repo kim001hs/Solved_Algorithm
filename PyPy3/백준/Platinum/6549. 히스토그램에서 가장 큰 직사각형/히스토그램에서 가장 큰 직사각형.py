5cc59d3232aca7bfa7ba7aebc2d48973ad474b31
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-15 17:04:50 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 180 ms, Memory: 140688 KB -BaekjoonHub

--- PyPy3/백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
+++ PyPy3/백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -16,15 +16,15 @@ while True:
             if stack[-1][1]<=histo[i]:
                 stack.append((i,histo[i]))
             else:
-                while stack and stack[-1][1] > histo[i]:
+                while stack and stack[-1][1] > histo[i]:#작은건 냅두기 벽으로 사용
                     prev=stack.pop()
                     if stack:
-                        width=i-stack[-1][0]-1
+                        width=i-stack[-1][0]-1#스택의 제일 위로 계산
                     else:
-                        width=i-1
+                        width=i-1#스택이 비었으면 i-1이 너비
                     max_=max(max_,width*prev[1])
                 stack.append((i,histo[i]))
-    while stack:
+    while stack:#마지막에 계속 올라가면 계산이 안되니까 한번 더 
         prev=stack.pop()
         if stack:
             width=n-stack[-1][0]
