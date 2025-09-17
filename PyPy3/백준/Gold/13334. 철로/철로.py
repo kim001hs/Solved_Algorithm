46c67e216df129bc61de5f6ddb2144d1d239c677
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-17 22:05:12 +0900

    [Gold II] Title: 철로, Time: 416 ms, Memory: 132020 KB -BaekjoonHub

--- PyPy3/백준/Gold/13334. 철로/철로.py
+++ PyPy3/백준/Gold/13334. 철로/철로.py
@@ -6,21 +6,21 @@ arr=[]
 arr2=[]
 for i in range(n):
     t1,t2=map(int,input().split())
-    if t2<t1:
+    if t2<t1:#작은 수를 왼쪽으로
         t1,t2=t2,t1
     arr2.append([t1,t2])
     
 d=int(input())
 for i in arr2:
-    if i[1]-i[0]<=d:
+    if i[1]-i[0]<=d:#d보다 거리가 길면 append 안함
         arr.append(i)
-arr.sort(key=lambda x:(x[1]))
+arr.sort(key=lambda x:(x[1]))#end 기준 정렬
 max_count=0
-heap=[]
+heap=[]#힙에서는 start기준 정렬
 for i in arr:
-    while heap and heap[0][0]<i[1]-d:
+    while heap and heap[0][0]<i[1]-d:#start가 가장 작은것부터 현재 end보다 작은 것들 pop해줌
         heapq.heappop(heap)
     
-    heapq.heappush(heap, i)
-    max_count=max(max_count,len(heap))
+    heapq.heappush(heap, i)#현재값 넣어줌
+    max_count=max(max_count,len(heap))#힙의 크기가 count
 print(max_count)
