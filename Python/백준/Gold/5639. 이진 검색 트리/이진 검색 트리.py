4268d105f8b223244ec98aa8d087869e1ae3447b
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-19 11:19:30 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 1932 ms, Memory: 34840 KB -BaekjoonHub

--- Python/백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ Python/백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,38 +1,31 @@
 import sys
-input = sys.stdin.readline
-#recursion error 방지
 sys.setrecursionlimit(10**9)
-
-arr = []
+def input(): return sys.stdin.readline()
+arr=[]
 while True:
     try:
-        x = int(input())
-        arr.append(x)
+        temp=int(input())
+        arr.append(temp)
     except:
         break
 
-
-def solution(A):
-    # 받은 배열 길이가 0이면 return
-    if len(A) == 0:
+def solve(start, end):
+    if start >= end:
         return
 
-    tempL, tempR = [], []
-    # 첫번째 값을 루트 노드로 설정
-    mid = A[0]
-    # 나머지 배열에 대해 for문을 돌면서 루트보다 커지는 부분을 기록해서 L과 R로 나눈다.
-    for i in range(1, len(A)):
-        if A[i] > mid:
-            tempL = A[1:i]
-            tempR = A[i:]
+    root = arr[start]
+    # root보다 커지는 첫 번째 요소의 인덱스를 찾음
+    divider_index = end
+    for i in range(start + 1, end):
+        if arr[i] > root:
+            divider_index = i
             break
-    else:
-    	#모두 mid보다 작은 경우
-        tempL = A[1:]
-    
-    #왼쪽, 오른쪽 순으로 재귀 후 루트 노드 값 출력
-    solution(tempL)
-    solution(tempR)
-    print(mid)
 
-solution(arr)
+    # 왼쪽 서브트리 (root보다 작은 값들)
+    solve(start + 1, divider_index)
+    # 오른쪽 서브트리 (root보다 큰 값들)
+    solve(divider_index, end)
+
+    print(root)
+    
+solve(0,len(arr))
