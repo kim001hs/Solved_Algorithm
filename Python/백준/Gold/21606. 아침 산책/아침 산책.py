f87f5252951ae55a34be81da5b705f2d0a6fc28d
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-19 20:56:34 +0900

    [Gold III] Title: 아침 산책, Time: 7596 ms, Memory: 66764 KB, Score: 98 point -BaekjoonHub

--- Python/백준/Gold/21606. 아침 산책/아침 산책.py
+++ Python/백준/Gold/21606. 아침 산책/아침 산책.py
@@ -11,9 +11,9 @@ for i in range(n-1):
     
 def dfs(visited, cur):
     global count
-    visited[cur]=True
+    visited |= (1<<cur)
     for i in graph[cur]:
-        if visited[i]==False:
+        if not (visited & (1<<i)):
             if (1<<i) & m:
                 count+=1
                 continue
@@ -21,7 +21,7 @@ def dfs(visited, cur):
     return
 
 for i in range(n):
-    visited=[False]*n
+    visited=0
     if (1<<i) & m:
         dfs(visited,i)
 print(count)
