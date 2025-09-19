e395c2a334d80dffab815e7edc2f6dbc224cbeb2
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-19 20:35:00 +0900

    [Gold III] Title: 아침 산책, Time: 40 ms, Memory: 32412 KB, Score: 60 point -BaekjoonHub

---
+++ Python/백준/Gold/21606. 아침 산책/아침 산책.py
@@ -0,0 +1,27 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+n=int(input())
+m=int(input()[::-1],2)
+graph=[[]for i in range(n)]
+count=0
+for i in range(n-1):
+    t1,t2=map(int, input().split())
+    graph[t1-1].append(t2-1)
+    graph[t2-1].append(t1-1)
+    
+def dfs(visited, cur):
+    global count
+    visited[cur]=True
+    for i in graph[cur]:
+        if visited[i]==False:
+            if (1<<i) & m:
+                count+=1
+                continue
+            dfs(visited,i)
+    return
+
+for i in range(n):
+    visited=[False]*n
+    if (1<<i) & m:
+        dfs(visited,i)
+print(count)
