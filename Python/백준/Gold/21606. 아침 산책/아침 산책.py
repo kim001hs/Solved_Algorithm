041dadd1ae55aeb2930f0b64776d9291b6120fde
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-20 00:21:52 +0900

    [Gold III] Title: 아침 산책, Time: 828 ms, Memory: 72148 KB, Score: 200 point -BaekjoonHub

--- Python/백준/Gold/21606. 아침 산책/아침 산책.py
+++ Python/백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,27 +1,48 @@
 import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
 def input(): return sys.stdin.readline().rstrip()
 n=int(input())
-m=int(input()[::-1],2)
+is_black=list(input())#0이 하얀색
+visited=[False]*n
 graph=[[]for i in range(n)]
 count=0
 for i in range(n-1):
     t1,t2=map(int, input().split())
     graph[t1-1].append(t2-1)
     graph[t2-1].append(t1-1)
-    
-def dfs(visited, cur):
-    global count
-    visited |= (1<<cur)
-    for i in graph[cur]:
-        if not (visited & (1<<i)):
-            if (1<<i) & m:
-                count+=1
-                continue
-            dfs(visited,i)
-    return
 
 for i in range(n):
-    visited=0
-    if (1<<i) & m:
-        dfs(visited,i)
+    temp=0
+    queue=deque()
+    if is_black[i]=='1':
+        for j in graph[i]:
+            if is_black[j]=='1':
+                count+=1
+    else:
+        if visited[i]:
+            continue
+        for j in graph[i]:
+            if is_black[j]=='1':
+                temp+=1
+            else:
+                if not visited[j]:
+                    queue.append(j)
+    visited[i]=True
+    while queue:
+        L=queue.popleft()
+        visited[L]=True
+        if is_black[L]=='1':
+            for j in graph[L]:
+                if is_black[j]=='1':
+                    count+=1
+        else:
+            for j in graph[L]:
+                if is_black[j]=='1':
+                    temp+=1
+                else:
+                    if not visited[j]:
+                        queue.append(j)
+    count+=temp*(temp-1)
+
 print(count)
