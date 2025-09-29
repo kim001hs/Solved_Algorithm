d921a36c2e972352984a3ac9c87909950a61a956
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-09-29 21:30:03 +0900

    [Gold I] Title: 외판원 순회, Time: 984 ms, Memory: 48856 KB -BaekjoonHub

---
+++ Python/백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+INF=int(1e9)
+n= int(input())
+graph=[]
+dp=[[-1]*(1<<n) for _ in range(n)]
+for i in range(n):
+    data=list(map(int, input().split()))
+    processed_data = [INF if d == 0 else d for d in data]
+    graph.append(processed_data)
+def dfs(start,visited):
+    if visited == (1<<n)-1:
+        dp[start][visited]=graph[start][0]
+        return graph[start][0]
+    if dp[start][visited]!=-1:
+        return dp[start][visited]
+    for i in range(n):
+        if visited & (1<<i):
+            continue
+        if dp[start][visited]==-1: 
+            dp[start][visited]=dfs(i, visited | (1<<i))+graph[start][i]
+        else:
+            dp[start][visited]=min(dp[start][visited], dfs(i, visited | (1<<i))+graph[start][i])
+    return dp[start][visited]
+
+print(dfs(0,1))
