bb7b9787cd07c118e3f79fe53a34e9a54964fdf5
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-10-02 23:10:42 +0900

    [Bronze V] Title: 개수 세기, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -0,0 +1,10 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+n=int(input())
+arr=list(map(int, input().split()))
+v=int(input())
+count=0
+for i in arr:
+    if i==v:
+        count+=1
+print(count)
