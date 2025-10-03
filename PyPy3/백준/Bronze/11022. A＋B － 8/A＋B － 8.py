81d3f13c13344a65f494465023449061432b4b7b
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-10-03 21:28:06 +0900

    [Bronze V] Title: A+B - 8, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,6 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+n=int(input())
+for i in range(n):
+    a,b=map(int, input().split())
+    print("Case #"+str(i+1)+": "+str(a)+" + "+str(b)+" = "+str(a+b))
