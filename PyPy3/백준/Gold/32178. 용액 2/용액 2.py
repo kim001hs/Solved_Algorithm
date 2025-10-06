2c76aede0a6d2ab29fd6d13d7ed0375bba5b1a14
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-10-07 01:41:25 +0900

    [Gold II] Title: 용액 2, Time: 1380 ms, Memory: 288616 KB -BaekjoonHub

--- PyPy3/백준/Gold/32178. 용액 2/용액 2.py
+++ PyPy3/백준/Gold/32178. 용액 2/용액 2.py
@@ -2,18 +2,24 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 INF=int(1e10)
 n=int(input())
-arr=list(map(int,input().split()))
+arr=[0]+list(map(int,input().split()))
 min_=INF
-sum_=[(0,0)]#부분합 배열
+for i in range(1,n+1):#부분합 배열로 만들기
+    arr[i]+=arr[i-1]
+ID=[i for i in range(n+1)]
+ID.sort(key=lambda x:arr[x])#부분합 배열 기준으로 인덱스 배열을 정렬
+
+res=int(2*1e9)
+
 for i in range(n):
-    sum_.append((sum_[-1][0]+arr[i],i+1))
-sum_.sort()
-for i in range(n):
-    temp=sum_[i+1][0]-sum_[i][0]
-    if abs(temp)<min_:
-        min_=abs(temp)
-        res=temp if sum_[i+1][1]>sum_[i][1] else -temp
-        fir,sec=min(sum_[i][1],sum_[i+1][1])+1,max(sum_[i][1],sum_[i+1][1])
-        
+    a,b=ID[i],ID[i+1]
+    if arr[b]-arr[a]<abs(res):
+        res=arr[b]-arr[a]
+        if a<=b:
+            left,right=a+1,b
+        else:
+            left,right=b+1,a
+            res*=-1
+
 print(res)
-print(str(fir)+' '+str(sec))
+print(left,right)
