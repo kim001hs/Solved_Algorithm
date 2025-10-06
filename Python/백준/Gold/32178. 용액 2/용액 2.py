f4755c203771924086fbb42661522c4cd853ccb2
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-10-07 01:44:39 +0900

    [Gold II] Title: 용액 2, Time: 1456 ms, Memory: 145580 KB -BaekjoonHub

---
+++ Python/백준/Gold/32178. 용액 2/용액 2.py
@@ -0,0 +1,25 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+INF=int(1e10)
+n=int(input())
+arr=[0]+list(map(int,input().split()))
+min_=INF
+for i in range(1,n+1):#부분합 배열로 만들기
+    arr[i]+=arr[i-1]
+ID=[i for i in range(n+1)]
+ID.sort(key=lambda x:arr[x])#부분합 배열 기준으로 인덱스 배열을 정렬
+
+res=int(2*1e9)
+
+for i in range(n):
+    a,b=ID[i],ID[i+1]
+    if arr[b]-arr[a]<abs(res):#arr[b]-arr[a]는 항상 0 이상
+        res=arr[b]-arr[a]
+        if a<=b:
+            left,right=a+1,b
+        else:#a,b 순서가 거꾸로면 바꾸고 res에 -1곱하기
+            left,right=b+1,a
+            res*=-1
+
+print(res)
+print(left,right)
