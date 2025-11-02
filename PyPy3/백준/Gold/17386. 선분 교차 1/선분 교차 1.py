d07785f3d2de4cd9f24695359f590ec7ce47ab3f
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-11-02 22:45:42 +0900

    [Gold III] Title: 선분 교차 1, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/17386. 선분 교차 1/선분 교차 1.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def ccw(x1, y1, x2, y2, x3, y3):
+    a = x1 * y2 + x2 * y3 + x3 * y1
+    b = y1 * x2 + y2 * x3 + y3 * x1
+    return a - b
+
+
+ax, ay, bx, by = map(int, input().split())
+cx, cy, dx, dy = map(int, input().split())
+
+abc = ccw(ax, ay, bx, by, cx, cy)
+abd = ccw(ax, ay, bx, by, dx, dy)
+cda = ccw(cx, cy, dx, dy, ax, ay)
+cdb = ccw(cx, cy, dx, dy, bx, by)
+
+if (abc * abd < 0) and (cda * cdb < 0):
+    print(1)
+elif (abc * abd == 0 and cda * cdb <= 0) or (abc * abd <= 0 and cda * cdb == 0):  # 한 점이 다른 선분 위에 있을 때
+    print(1)
+else:
+    print(0)
