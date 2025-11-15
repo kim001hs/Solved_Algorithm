994d2e3c86c2feebbb8f9d6e35d95d211164c872
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-11-15 13:01:13 +0900

    [Bronze IV] Title: 주사위 세개, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+a, b, c = map(int, input().split())
+if a == b:
+    if a == c:
+        print(10000+a*1000)
+    else:
+        print(1000+b*100)
+elif b == c:
+    print(1000+b*100)
+elif a == c:
+    print(1000+a*100)
+else:
+    print(max(a, b, c)*100)
