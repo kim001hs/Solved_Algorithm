67eceb32a9e64fdea40a202613c740ed8ba9233d
김현서 <80082902+kim001hs@users.noreply.github.com>
2025-11-20 23:29:42 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 104 ms, Memory: 111088 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+for i in range(n):
+    for j in range(i):
+        print(" ", end="")
+    for j in range(n, i, -1):
+        print("*", end="")
+    print()
